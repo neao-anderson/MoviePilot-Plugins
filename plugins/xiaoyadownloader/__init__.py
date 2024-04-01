c8deeea7a2e96d1183f5c710f5b93d2ddc767d17
neao-anderson <neao@sina.com>
2024-04-01 14:53:17 +0800

    v0.2

--- plugins/xiaoyadownloader/__init__.py
+++ plugins/xiaoyadownloader/__init__.py
@@ -1,14 +1,14 @@
 from typing import List, Tuple, Dict, Any
 
-from python_hosts import Hosts, HostsEntry
-
 from app.core.event import eventmanager
 from app.log import logger
 from app.plugins import _PluginBase
 from app.schemas.types import EventType
-from app.utils.ip import IpUtils
-from app.utils.system import SystemUtils
 
+from threading import Thread
+import urllib.parse, requests, re, os, time
+from webdav3.client import Client
+from webdav3.exceptions import *
 
 class xiaoyadownloader(_PluginBase):
     # 插件名称
@@ -32,13 +32,18 @@ class xiaoyadownloader(_PluginBase):
 
     # 私有属性
     _urls = []
+    _save_path = ''
+    _options = {}
     _enabled = False
+    
 
     def init_plugin(self, config: dict = None):
         # 读取配置
         if config:
             self._enabled = config.get("enabled")
+            self._save_path = config.get("save_path")
             self._urls = config.get("urls")
+
             # 还需要读取webdav配置
             
             if isinstance(self._urls, str):
@@ -49,20 +54,24 @@ class xiaoyadownloader(_PluginBase):
                 new_urls = []
                 for url in self._urls:
                     if url and url != '\n':
+                        # 对URL进行解码
+                        url = urllib.parse.unquote(url)
                         new_urls.append(url.replace("\n", ""))
+                        logger.debug(f"[XIAOYA]解码后的URL:%s" % url)
                 self._urls = new_urls
 
                 # 进行下载
-                error_flag, error_urls = self.__xiaoya_downloader(self._urls)
+                error_flag, error_urls = self.__xiaoya_downloader(self._urls, self._save_path)
 
                 # 只执行一次
                 self._enabled = self._enabled and not error_flag
 
                 # 更新错误urls
                 self.update_config({
+                    "enabled": self._enabled,
+                    "save_path": self._save_path,
                     "urls": '\n'.join(self._urls),
-                    "err_urls": '\n'.join(self.err_urls),
-                    "enabled": self._enabled
+                    "err_urls": '\n'.join(error_urls)
                 })
 
     def get_state(self) -> bool:
@@ -98,6 +107,29 @@ class xiaoyadownloader(_PluginBase):
                                                'props': {
                                                    'model': 'enabled',
                                                    'label': '启用插件',
+                                               }
+                                           }
+                                       ]
+                                   }
+                               ]
+                           },
+                           {
+                               'component': 'VRow',
+                               'content': [
+                                   {
+                                       'component': 'VCol',
+                                       'props': {
+                                           'cols': 12
+                                       },
+                                       'content': [
+                                           {
+                                               'component': 'VTextarea',
+                                               'props': {
+                                                   'model': 'save_path',
+                                                   'label': '保存路径',
+                                                   'value': '/media',
+                                                   'rows': 1,
+                                                   'placeholder': '请输入保存路径',
                                                }
                                            }
                                        ]
@@ -182,70 +214,182 @@ class xiaoyadownloader(_PluginBase):
     def get_page(self) -> List[dict]:
         pass
 
-    @staticmethod
-    def __read_system_hosts():
-        """
-        读取系统hosts对象
-        """
-        # 获取本机hosts路径
-        if SystemUtils.is_windows():
-            hosts_path = r"c:\windows\system32\drivers\etc\hosts"
-        else:
-            hosts_path = '/etc/hosts'
-        # 读取系统hosts
-        return Hosts(path=hosts_path)
+    def parse_url(self, url):
+        # 根据URL解析主机
+        pattern = r'^(?:http[s]?://)?([^:/\s]+)(?::(\d+))?'
+        match = re.search(pattern, url)
+        host = match.group(0)
+        logger.debug(f"[XIAOYA]主机:{str(host)}")
 
-    def __xiaoya_downloader(self, urls):
+        # 根据URL解析资源路径
+        remote_path = url.replace(host,'')
+        if remote_path.startswith('/d'):
+            remote_path = remote_path[2:]
+        remote_path = "/dav" + remote_path
+        logger.debug(f"[XIAOYA]远程路径:{str(remote_path)}")
+        
+        # 设置保存文件夹
+        save_floder = os.path.basename(url) # 最后一级目录
+        logger.debug(f"[XIAOYA]保存文件夹:{str(save_floder)}")
+        
+        return {"remote_path": remote_path, "save_floder": save_floder}
+
+    def list_remote(self, client, remote_path):
+        # 获取资源列表
+        try:
+            items = client.list(remote_path,get_info=True)
+        except Exception as e:
+            logger.error(f"[XIAOYA]获取远程文件列表时出错: {str(e)}")
+        
+        file_list = []
+        
+        for item in items:
+            if item['isdir']: # 如果是文件夹，继续打开
+                file_list += self.list_remote(client, item['path'])
+            else: #对于文件直接进行下载
+                _file_name, _ext = os.path.splitext(item['path'])
+                file_list.append({
+                    "name": item['name'],
+                    "size": item['size'],
+                    "remote_path": item['path'],
+                    "file_type": _ext[1:],
+                })
+        return file_list 
+
+    def download_file(self, item, retries=5, delay=3):
+        file_name   = item['name']
+        file_size   = int(item['size'])
+        remote_path = item['remote_path']
+        save_path   = item["save_path"]
+
+        if retries <= 0:
+            logger.error(f"[XIAOYA]{str(file_name)}下载失败:")
+            return 0
+
+        if os.path.exists(save_path):
+            _file_size = os.path.getsize(save_path)
+            write_mode = 'ab'
+            logger.info(f"[XIAOYA]开始续传文件{str(file_name)}")
+        else:
+            _file_size = 0
+            write_mode = 'wb'
+            logger.info(f"[XIAOYA]开始下载文件{str(file_name)}")
+
+        try:
+            headers = {'Range': 'bytes=%d-' % _file_size}
+            with requests.get(self._options['webdav_hostname'] + remote_path, stream=True, auth=(self._options['webdav_login'], self._options['webdav_password']), headers=headers) as r:
+                with open(save_path, write_mode) as f:
+                    next_level = max(1, int(_file_size/file_size*10))*10
+                    for chunk in r.iter_content(chunk_size = 1024*32 ):
+                        if chunk:
+                            f.write(chunk)
+                            f.flush()
+                            _file_size += len(chunk)
+                            progress = round(_file_size/file_size*100,2)
+                            if progress >= next_level and file_size/(1024*1024*1024) > 1:
+                                next_level += 10
+                                logger.info(f"[XIAOYA]文件 {str(file_name)} 下载进度: {str(progress)}%,{str(round(_file_size/(1024*1024*1024),2))}GB/{str(round(file_size/(1024*1024*1024),2))}GB")
+            logger.info(f"[XIAOYA]文件已从 {str(remote_path)} 下载到 {str(save_path)}")
+            return 1
+        except requests.exceptions.RequestException as e:
+            # 捕获所有requests库的异常
+            logger.error(f"[XIAOYA]出现下载异常: {e}")
+            time.sleep(delay)
+            return self.download_file(item, retries-1, delay*2)
+        except IOError as e:
+            # 捕获文件操作相关的异常
+            logger.error(f"[XIAOYA]出现读写异常: {e}")
+        except Exception as e:
+            # 捕获其他未知异常
+            logger.error(f"[XIAOYA]出现未知异常: {e}")
+
+    def download_files(self, download_file_list):
+        total_file_num  = len(download_file_list)
+        finish_file_num = 0
+        # socketio.emit('update_list_progress', {'total_file_num': total_file_num, 'finish_file_num': finish_file_num})
+        logger.info(f"总共需要下载{str(total_file_num)}个文件,已经下载{str(finish_file_num)}个文件")
+        
+        for item in download_file_list:
+            file_name   = item['name']
+            file_size   = int(item['size'])
+            remote_path = item['remote_path']
+            save_path   = item["save_path"]
+
+            floder_path = os.path.dirname(save_path)
+            if not os.path.exists(floder_path):
+                os.makedirs(floder_path)
+
+            if not os.path.exists(save_path) or os.path.getsize(save_path) < file_size:
+                finish_file_num += self.download_file(item)
+            else:
+                finish_file_num += 1
+                logger.warn(f"[XIAOYA]文件已存在")
+            
+            # socketio.emit('update_list_progress', {'total_file_num': total_file_num, 'finish_file_num': finish_file_num})
+            logger.info(f"[XIAOYA]总共需要下载{str(total_file_num)}个文件,已经下载{str(finish_file_num)}个文件")
+        
+        logger.info(f"[XIAOYA]文件全部下载任务完成")
+
+    def xiaoya_downloader(self, url, save_path):
+        temp = self.parse_url(url)
+        url = temp["url"]
+        host = temp["host"]
+        remote_path = temp["remote_path"]
+        save_floder = temp["save_floder"]
+        
+        # WebDAV 连接配置
+        self._options = {
+            'webdav_hostname': host,
+            'webdav_login':    "guest",
+            'webdav_password': "guest_Api789",
+            'disable_check': True,
+        }
+        # 创建客户端实例
+        client = Client(self._options)
+        
+        download_file_list = []
+
+        #此处可以设置筛选规则
+        item = ''
+        try:
+            item = client.info(remote_path)
+        except Exception as e:
+            logger.error(f"[XIAOYA]获取文件信息时出错: {str(e)}")
+        
+        # 判断是文件还是文件夹
+        if item["size"]:
+            _file_name, _ext = os.path.splitext(save_floder)
+            download_file_list=[{
+                    "name": item['name'],
+                    "size": item['size'],
+                    "remote_path": remote_path,
+                    "file_type": _ext[1:],
+                    'save_path': save_path + save_floder
+                }]
+        else:
+            download_file_list = list_remote(client, remote_path)
+            for item in download_file_list:
+                item["save_path"] =  item["remote_path"].replace(remote_path,save_path+save_floder)
+
+        self.download_files(download_file_list)
+        self.systemmessage.put(f"[XIYA] {str(err)}下载完成")
+
+    @staticmethod
+
+    def __xiaoya_downloader(self, urls, save_path):
         """
         逐一下载每个URL
         """
-        # # 系统hosts对象
-        # system_hosts = self.__read_system_hosts()
-        # # 过滤掉插件添加的hosts
-        # orgin_entries = []
-        # for entry in system_hosts.entries:
-        #     if entry.entry_type == "comment" and entry.comment == "# CustomHostsPlugin":
-        #         break
-        #     orgin_entries.append(entry)
-        # system_hosts.entries = orgin_entries
-        # # 新的有效hosts
-        # new_entrys = []
-        # # 新的错误的hosts
         err_urls = []
         err_flag = False
         for index, url in enumerate(urls):
-            # if not url:
-            #     continue
-            # host_arr = str(host).split()
             try:
-                # host_entry = HostsEntry(entry_type='ipv4' if IpUtils.is_ipv4(str(host_arr[0])) else 'ipv6',
-                #                         address=host_arr[0],
-                #                         names=host_arr[1:])
-                # new_entrys.append(host_entry)
-                if index % 2 == 0:
-                    err_urls.append(url)
-                    logger.error(f"[XIAOYA] URL错误：{str(url)}")
-                    err_flag = False
-            except Exception as err:
+                xiaoya_downloader(url, save_path)
+            except Exception as e:
                 err_urls.append(url + "\n")
-                logger.error(f"[HOST] 格式转换错误：{str(err)}")
+                logger.error(f"[XIAOYA] {str(e)}下载失败")
                 # 推送实时消息
-                self.systemmessage.put(f"[HOST] 格式转换错误：{str(err)}")
-
-        # # 写入系统hosts
-        # if new_entrys:
-        #     try:
-        #         # 添加分隔标识
-        #         system_hosts.add([HostsEntry(entry_type='comment', comment="# CustomHostsPlugin")])
-        #         # 添加新的Hosts
-        #         system_hosts.add(new_entrys)
-        #         system_hosts.write()
-        #         logger.info("更新系统hosts文件成功")
-        #     except Exception as err:
-        #         err_flag = True
-        #         logger.error(f"更新系统hosts文件失败：{str(err) or '请检查权限'}")
-        #         # 推送实时消息
-        #         self.systemmessage.put(f"更新系统hosts文件失败：{str(err) or '请检查权限'}")
+                self.systemmessage.put(f"[XIAOYA]{str(e)}下载失败")
         return err_flag, err_urls
 
     def stop_service(self):
